6fd54b8b2d9123dc250fd1d8693695b816189552
devin <devin@samir.co.id>
2026-01-19 11:12:54 +0700

    Initial Commit

---
+++ PennyWise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PennyWise
+//
+//  Created by Samir iOS on 19/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
